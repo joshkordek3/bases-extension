46c2b79fe551696c824914ee176fc9b743219ec9
joshkordek3 <79750252+joshkordek3@users.noreply.github.com>
2021-03-15 17:42:44 +0800

    fixed placing glitch I think

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% color=#800080 weight=100 icon="\uf1ec" block="Advanced Numbers"
+//% color=#800080 weight=101 icon="\uf1ec" block="Advanced Numbers"
 namespace hello {
 //%block
 //% group="Converting Bases"
