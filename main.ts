e3507ba90fbd3846e07183a6aeca2900e3752688
joshkordek3 <79750252+joshkordek3@users.noreply.github.com>
2021-03-16 10:24:56 +0800

    added measuring and made sure base is within range (binary-10)

--- main.ts
+++ main.ts
@@ -4,10 +4,17 @@ namespace advanced.math {
 //% group="Converting Bases"
 export function convert_from_base_to_base_10 (base: number, num: number) {
     temp_val = 0
-    for (let index = 0; index <= convertToText(num).length - 1; index++) {
-        temp_val += parseFloat(convertToText(num).charAt(convertToText(num).length - (index + 1))) * base ** index
+    if (is_between(base, 2, 10)) {
+        for (let index = 0; index <= convertToText(num).length - 1; index++) {
+            temp_val += parseFloat(convertToText(num).charAt(convertToText(num).length - (index + 1))) * base ** index
+        }
     }
     return temp_val
 }
 let temp_val = 0
+//%block
+//% group="Measuring"
+export function is_between (num: number, least: number, max: number) {
+    return least >= num && num <= max
+}
 }
