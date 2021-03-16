e6cfc2222ad45faf3a7d615f1ada604b48ba2066
joshkordek3 <79750252+joshkordek3@users.noreply.github.com>
2021-03-16 11:28:53 +0800

    added converting from base 10 and fixed the "is_between" block

--- main.ts
+++ main.ts
@@ -15,6 +15,32 @@ let temp_val = 0
 //%block
 //% group="Measuring"
 export function is_between (num: number, least: number, max: number) {
-    return least >= num && num <= max
+    return least <= num && num <= max
 }
+//%block
+//% group="Converting Bases"
+export function convert_from_base_10_to_base (num: number, base: number) {
+    temp_val2 = num
+    temp_val = 0
+    index3 = 0
+    if (is_between(base, 2, 10)) {
+        while (!(divider > num)) {
+        divider = base ** index3
+        index3 += 1
+        }
+        index3 = 0
+        for (let index2 = 0; index2 <= 1e+300; index2++) {
+            temp_val = parseFloat("" + convertToText(temp_val) + convertToText(Math.idiv(temp_val2, divider)))
+            temp_val2 = temp_val2 % divider
+            divider = divider / base
+            if (num <= convert_from_base_to_base_10 (base, temp_val)) {
+            break;
+            }
+        }
+    }
+    return temp_val
+}
+let divider = 0
+let index3 = 0
+let temp_val2 = 0
 }
