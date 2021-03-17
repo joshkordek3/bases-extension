e0e7d37d5d3dff55bd134a0b33110a5b668e1e5d
joshkordek3 <79750252+joshkordek3@users.noreply.github.com>
2021-03-17 15:15:35 +0800

    fixed glitches

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ export function convert_from_base_to_base_10 (base: number, num: number) {
 export function convert_from_hex_base_10 (num: string) {
     temp_num = 0
     for (let index = 0; index <= num.length - 1; index++) {
-        temp_num += ((a_n().indexOf(num.charAt(index))) ** (index + 1))
+        temp_num += ((a_n().indexOf(num.charAt(index))) * 16 ** index)
     }
     return temp_num
 }
